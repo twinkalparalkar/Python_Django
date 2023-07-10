18af806d20decb97d7aaf02fd2c08d9a5ac489d3
twinkalp <twinkalparalkar2@gmail.com>
2023-07-10 10:06:49 +0530

    Initial2 commit

--- question/admin.py
+++ question/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Topic)
+
+
+class AnswerAdmin(admin.StackedInline):
+    model=Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines=[AnswerAdmin]
+     
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Answer)
--- question/models.py
+++ question/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class BaseModel(models.Model):
+    uid=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    created_at=models.DateField(auto_now_add=True)
+    updated_at=models.DateField(auto_now=True)
+
+    class Meta:
+        abstract=True#to inherit
+
+class Topic(BaseModel):
+    topic_name=models.CharField(max_length=100)
+    
+
+class Question(BaseModel):
+    topic=models.ForeignKey(Topic,on_delete=models.CASCADE,related_name='topic')
+    question_name=models.CharField(max_length=100)
+    marks=models.IntegerField(default=5)
+
+class Answer(BaseModel):
+    question=models.ForeignKey(Question,on_delete=models.CASCADE,related_name='question_answer')
+    answer=models.CharField(max_length=100)
+    is_correct=models.BooleanField(default=False)
