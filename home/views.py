a48472bedeb264f9755909e86172a5cb8a1242a0
twinkalp <twinkalparalkar2@gmail.com>
2023-07-11 11:16:02 +0530

    Initial1 commit

--- home/views.py
+++ home/views.py
@@ -2,7 +2,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view()#API decoretor
+@api_view(['GET','POST','PATCH'])#API decoretor
 def home(request):
     response={'status':200,'message':"hi,I am DRF"}
+    if request.method=="POST":
+        print('POST')
+    elif request.method=="GET":
+        print('GET')
     return Response(response)
+
+
