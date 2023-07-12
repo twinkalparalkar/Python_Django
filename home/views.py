aab3b32514a4c204d7405e9ee68ff8417d760717
twinkalp <twinkalparalkar2@gmail.com>
2023-07-12 09:46:44 +0530

    getpostapi commit

--- home/views.py
+++ home/views.py
@@ -1,6 +1,25 @@
 # from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import *
+from .serializer import *
+@api_view(['GET'])#API decoretor
+def get_students(request):
+    response={'status':200,'message':"hi,I am DRF"}
+    student_objs=Student.objects.all()
+    serializer=StudentSerializer(student_objs,many=True)
+    response['data']=serializer.data
+    return Response(response)
+
+@api_view(['POST'])#API decoretor
+def post_students(request):
+    response={'status':200,'message':"hi,I am DRF"}
+    data=request.data
+    serializer=StudentSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(response)
+    return Response(serializer.errors)
 
 @api_view(['GET','POST','PATCH'])#API decoretor
 def home(request):
--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from home.views import home
-
+from home.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home,name="home"),
+    path('getstudent',get_students,name="get_api"),
+    path('poststudent',post_students,name="post_api")
 ]
